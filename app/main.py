2d3fc32add766c03edda944c3b7c7ba18d46a253
zachhew <zaharikafona@gmail.com>
2026-03-23 23:13:25 +0300

    update main

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 
 from app.api.routes import ask
+from app.core.logging import setup_logging
 
+setup_logging()
 app = FastAPI(
     title = "Advanced RAG support",
     description="Advanced RAG support",
